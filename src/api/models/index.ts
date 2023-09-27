534a9f289a2d77c9b3764072ec9afd3a3d3ddeaf
cosmic-fox <arya@cosmicfox.io>
2023-09-27 19:38:14 +0200

    chore: some more template cleanup

--- src/api/models/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./create-todo-dto";
-export * from "./todo";
-export * from "./update-todo-dto";
--- src/components/layouts/defaultLayout.tsx
+++ src/components/layouts/defaultLayout.tsx
@@ -1,12 +1,6 @@
 import { FC, PropsWithChildren } from "react";
-import { Navbar } from "../navigation/navbar/navbar.tsx";
 
 interface DefaultLayoutProps extends PropsWithChildren {}
 export const DefaultLayout: FC<DefaultLayoutProps> = ({ children }) => {
-    return (
-        <>
-            <Navbar />
-            {children}
-        </>
-    );
+    return <>{children}</>;
 };
